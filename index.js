a7dd52e21aefb61515436203163813231da5c12d
mcerrillo1 <48847969+mcerrillo1@users.noreply.github.com>
2019-05-23 11:19:50 -0700

    Update index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+function numFunc (integer){
+  console.log (integer)
+  if (integer % 3 == 0 || integer % 5 == 0){
+    return true;
+    }
+   else {
+    return false;
+    }
+  }
+  
+  numFunc (15);
